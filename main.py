ca1c952604b5ec224e8fc34ebc5e0d87319d739a
Alexander Petrosyan <modernpacifist@gmail.com>
2020-01-26 16:59:23 +0300

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PIL import Image, ImageDraw
+import math
 
-# static test pattern (*Rhombus (band) 1 [line 26])
+
+# *Hatch 1x45
 # first value   = angle
 # second value  = starting line point X Coordinate
 # third value   = starting line point Y Coordinate
@@ -8,26 +10,33 @@ from PIL import Image, ImageDraw
 # fifth value   = next line shift perp. to line direction (angle +90°)
 # sixth value   = mark length
 # seventh value = gap length (usually negative)
-_pattern = [(60, 0, 0, 1.5, 0.8660254, 2, -2),
-            (120, 1, 0, 0.5, 0.8660254, 2, -2)]
+
+def get_rotation(angle):
+    return angle
 
 
 # main function for drawing
-def draw_by_pattern(img_obj):
+def draw_by_pattern(img_obj, pattern_list):
     drawing_object = ImageDraw.Draw(img_obj)
-    #                   x1, y1, x2, y2
-    drawing_object.line((0, 0, 300, 300), fill=128, width=1)
-    drawing_object.line((0, 300, 300, 0), fill=128, width=1)
+
+    for obj in pattern_list:
+        # x1, y1, x2, y2
+        print(get_rotation(obj[0]))
+        # drawing_object.line((0, 0, 0, 1), fill=0, width=1)
+        # drawing_object.line((0, 300, 300, 0), fill=0, width=1)
 
     del drawing_object
     return None
 
 
 # create an object-image
-image_object = Image.new('RGB', (300, 300), (255, 255, 255))
+size = (20, 20)
+white_color = (255, 255, 255)
+image_object = Image.new('RGB', size, white_color)
 
 # draw lines
-draw_by_pattern(image_object)
+_pattern = [(45, 0, 0, 0, 1)]
+draw_by_pattern(image_object, _pattern)
 
 # save changes to object-image
 image_object.save("test.jpeg", "jpeg")
