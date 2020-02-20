8bc0ed57aa14133bcbfca963947e0d8e04ec6d6a
Alexander Petrosyan <modernpacifist@gmail.com>
2020-02-20 21:55:41 +0300

    added vector

--- paint_lib.py
+++ paint_lib.py
@@ -10,13 +10,15 @@ def draw_by_pattern_JPEG(test_pattern, img_size, background_color):
     local_image = Image.new('RGB', img_size, background_color)
     drawing_object = ImageDraw.Draw(local_image)
 
-    local_pattern = PatternLine(test_pattern[0], test_pattern[1], test_pattern[2], test_pattern[3],
-                                test_pattern[4], test_pattern[5], test_pattern[6])
+    null_vector = Vector(0, 0)
+
+    local_pattern = PatternLine(test_pattern[0], null_vector,  test_pattern[2], test_pattern[3],
+                                test_pattern[4], test_pattern[5])
 
     x_y = math_lib.rotate_vector_sin_cos(img_size[0], local_pattern.angle)
 
-    null_vector = Vector(0, 0)
     scaled_vector = Vector(x_y[0], x_y[1])
+
     # basis scaled_vector
     drawing_object.line((null_vector.x, null_vector.y, scaled_vector.x, scaled_vector.y), fill = 255, width = 10)
     # opposite direction to the scaled_vector
--- patternline.py
+++ patternline.py
@@ -1,7 +1,7 @@
 class PatternLine:
-    def __init__(self, angle, x, y, along, perpendicular, length, gap):
+    def __init__(self, angle, vector, along, perpendicular, length, gap):
         self.angle = angle
-        self.origin = {x, y}
+        self.origin = vector
         self.line_shift_along = along
         self.line_shift_perp = perpendicular
         self.dash = [length, gap]
